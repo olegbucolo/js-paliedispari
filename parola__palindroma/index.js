0b1167a2281b80f3ab6af84bca223e64544470e5
olegbucolo <olegbucolo97@gmail.com>
2025-12-05 22:09:34 +0100

    fix: rimosso codice ridontante e assegnato valore return alla funzione isPalindrome(word)

--- parola__palindroma/index.js
+++ parola__palindroma/index.js
@@ -1,29 +1,18 @@
 let parola = prompt(`Inserisci una parola per vedere se e' palindroma`);
-
-isPalindrome(parola);
+console.log(`La parola che hai scelto e' "${parola}"`)
+console.log(isPalindrome(parola));
 
 function isPalindrome(word) {
-    // la variabile f comincera' con il valore dell'indice dell'ultima lettera della parola
-    let f = parola.length - 1;
-    // inizialmente il valore di palindroma sara' false
-    let palindroma = false;
-    // loop fino a meta' della parola. Se la parola e' dispari viene arrotondato per eccesso di default
-    for (let i = 0; i < parola.length / 2; i++) {
-        console.log('i', i, 'f', f)
-        // se la lettera all'inizio e' diversa dalla lettera alla fine metto palindroma = false e interrompo
-        // altrimenti palindroma = true
-        if (parola[i] !== parola[f]) {
-            palindroma = false;
-            console.log(`le lettere su indice ${i} e ${f} non corrispondono, interrompo il ciclo`)
-            break;
-        } else {
-            console.log(`le lettere su indice ${i} e ${f} corrispondono`)
-            palindroma = true;
+    // la variabile indexEnd comincera' con il valore dell'indice dell'ultima lettera della parola
+    let indexEnd = parola.length - 1;
+    // loop da ambo i lati confrontando le lettere iniziali e finali
+    for (let indexStart = 0; indexStart < parola.length / 2; indexStart++) {
+        if (parola[indexStart] !== parola[indexEnd - indexStart]) {
+            console.log(`le lettere su indice ${indexStart} e ${indexEnd - indexStart} non corrispondono, interrompo il ciclo`)
+            return false
         }
-        // decremento l'indice del fine parola
-        f--;
     }
-    palindroma == false
-        ? (console.log(`parola palindroma: %c${palindroma}`, "color: red"))
-        : console.log(`parola palindroma: %c${palindroma}`, "color: green")
+
+    return true
+
 }
