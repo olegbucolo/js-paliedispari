d990a9468d71914d8b9dfc3142bd60f05c415701
olegbucolo <olegbucolo97@gmail.com>
2025-12-05 15:51:46 +0100

    refactor: in index.js nella cartella parola__palindroma ho messo la logica nella funzione

--- parola__palindroma/index.js
+++ parola__palindroma/index.js
@@ -1,26 +1,29 @@
 let parola = prompt(`Inserisci una parola per vedere se e' palindroma`);
 
-// la variabile f comincera' con il valore dell'indice dell'ultima lettera della parola
-let f = parola.length - 1;
-// inizialmente il valore di palindroma sara' false
-let palindroma = false;
-// loop fino a meta' della parola. Se la parola e' dispari viene arrotondato per eccesso di default
-for (let i = 0; i < parola.length / 2; i++) {
-    console.log('i', i, 'f', f)
-    // se la lettera all'inizio e' diversa dalla lettera alla fine metto palindroma = false e interrompo
-    // altrimenti palindroma = true
-    if (parola[i] !== parola[f]) {
-        palindroma = false;
-        console.log(`le lettere su indice ${i} e ${f} non corrispondono, interrompo il ciclo`)
-        break;
-    } else {
-        console.log(`le lettere su indice ${i} e ${f} corrispondono`)
-        palindroma = true;
-    }
-    // decremento l'indice del fine parola
-    f--;
-}
+isPalindrome(parola);
 
-palindroma == false
-    ? (console.log(`parola palindroma: %c${palindroma}`, "color: red"))
-    : console.log(`parola palindroma: %c${palindroma}`, "color: green")
+function isPalindrome(word) {
+    // la variabile f comincera' con il valore dell'indice dell'ultima lettera della parola
+    let f = parola.length - 1;
+    // inizialmente il valore di palindroma sara' false
+    let palindroma = false;
+    // loop fino a meta' della parola. Se la parola e' dispari viene arrotondato per eccesso di default
+    for (let i = 0; i < parola.length / 2; i++) {
+        console.log('i', i, 'f', f)
+        // se la lettera all'inizio e' diversa dalla lettera alla fine metto palindroma = false e interrompo
+        // altrimenti palindroma = true
+        if (parola[i] !== parola[f]) {
+            palindroma = false;
+            console.log(`le lettere su indice ${i} e ${f} non corrispondono, interrompo il ciclo`)
+            break;
+        } else {
+            console.log(`le lettere su indice ${i} e ${f} corrispondono`)
+            palindroma = true;
+        }
+        // decremento l'indice del fine parola
+        f--;
+    }
+    palindroma == false
+        ? (console.log(`parola palindroma: %c${palindroma}`, "color: red"))
+        : console.log(`parola palindroma: %c${palindroma}`, "color: green")
+}
