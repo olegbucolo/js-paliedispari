a49a8ca2683d85f0242e9bd7108a3d1a4d44e1c4
olegbucolo <olegbucolo97@gmail.com>
2025-12-05 15:46:21 +0100

    refactor: in index.js di parola__palindroma aggiungo dei commenti per spiegare il procedimento

--- parola__palindroma/index.js
+++ parola__palindroma/index.js
@@ -1,10 +1,15 @@
 let parola = prompt(`Inserisci una parola per vedere se e' palindroma`);
 
+// la variabile f comincera' con il valore dell'indice dell'ultima lettera della parola
 let f = parola.length - 1;
+// inizialmente il valore di palindroma sara' false
 let palindroma = false;
+// loop fino a meta' della parola. Se la parola e' dispari viene arrotondato per eccesso di default
 for (let i = 0; i < parola.length / 2; i++) {
     console.log('i', i, 'f', f)
-    if (!(parola[i] === parola[f])) {
+    // se la lettera all'inizio e' diversa dalla lettera alla fine metto palindroma = false e interrompo
+    // altrimenti palindroma = true
+    if (parola[i] !== parola[f]) {
         palindroma = false;
         console.log(`le lettere su indice ${i} e ${f} non corrispondono, interrompo il ciclo`)
         break;
@@ -12,6 +17,7 @@ for (let i = 0; i < parola.length / 2; i++) {
         console.log(`le lettere su indice ${i} e ${f} corrispondono`)
         palindroma = true;
     }
+    // decremento l'indice del fine parola
     f--;
 }
 
